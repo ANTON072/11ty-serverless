22e36a8e53dce703e381afccf2eca71ba8b39ad0
Katsushi OUGI <ougi@to-r.net>
2023-10-27 21:34:54 +0900

    サーバーレスページの追加

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,21 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+
 module.exports = (config) => {
   config.setNunjucksEnvironmentOptions({
     throwOnUndefined: true,
     autoescape: false,
-  })
+  });
 
   config.addPassthroughCopy("src/assets/img/");
   config.addPassthroughCopy("src/assets/js/");
 
   config.addWatchTarget("./src/assets/scss/");
   config.setWatchThrottleWaitTime(500);
+
+  config.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "serverless",
+    functionsDir: "./netlify/functions/serverless",
+  });
 
   return {
     dir: {
@@ -17,4 +24,4 @@ module.exports = (config) => {
       includes: "_includes",
     },
   };
-}
+};
